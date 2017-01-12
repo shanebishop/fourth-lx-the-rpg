75bc4ff05469c8af4e0ab6628124e099b29a5a88
Shane Bishop <shanebishop@users.noreply.github.com>
2017-01-12 18:46:16 -0500

    Create Character.java

---
+++ Character.java
@@ -0,0 +1,16 @@
+public class Character {
+  private String class;
+  private int strength;
+  private int speed;
+  private int hp;
+  private int defense;
+  private String name;
+  private String race;
+  private int mana;
+  private int level;
+  private int exp;
+  
+  public Character() {
+    
+  }
+}
