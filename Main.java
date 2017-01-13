2600e633351a3dd437650c516647926032c2fc6e
Shane Bishop <shanebishop@users.noreply.github.com>
2017-01-12 19:04:55 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+  // Instance variables
+  
+  // Constructor
+  public Main() {
+  }
+  
+  // Methods
+  
+  // Main method
+  public static void main(String[] args) {
+    
+  }
+}
